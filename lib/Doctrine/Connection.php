fff959d174285f23613d2fd483588fb04bc91c30
Jeff Hanke <geophree@wikispaces.com>
2010-12-07 16:10:47 -0800

    Connection clear: check for repository before evictAll-ing it.

--- lib/Doctrine/Connection.php
+++ lib/Doctrine/Connection.php
@@ -1236,7 +1236,9 @@ abstract class Doctrine_Connection extends Doctrine_Configurable implements Coun
     public function clear()
     {
         foreach ($this->tables as $k => $table) {
-            $table->getRepository()->evictAll();
+            if ($table->getRepository()) {
+                $table->getRepository()->evictAll();
+            }
             $table->clear();
         }
     }
